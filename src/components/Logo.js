0245be624f39eaa39891eaaeb5d599399900ac71
Kanokpit165 <ra.kanokpit_st@tni.ac.th>
2021-12-04 10:49:34 +0700

    Workshop Day 4

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,19 +1,23 @@
 import React from 'react'
- import {logo, title} from '../styles/style'
+import { logo , title } from './../styles/style'
+import useHover from '../hooks/useHover'
 
- const Logo = () =>{
+const Logo = () => {
 
-    const logoImage = './logo192.png'
-    // const logoImage = {
-    //     url : './logo192.png'
-    // }
-
-     return (
-         <div>
-             <h3 style={title}>Logo</h3>
-             {/* <img src={logoImage} width="100" alt='logo' /> */}
-             <img style={logo} src={logoImage} width="100" alt='logo' />
-         </div>
-     )
+    const [hover , mouseOver , mouseOut] = useHover()
+    // const logoImage = "./logo192.png"
+    const logoImage = {
+        url: './logo192.png'
     }
-    export default Logo;
+    return (
+        <div>
+            <h3 style={title}>Logo</h3>
+            {
+                hover ? <h3>Logo</h3> : null
+            }
+            {/* <img src={logoImage} width="100" alt="logo" /> */}
+            <img onMouseOut={mouseOut} onMouseOver={mouseOver} style={logo} src={logoImage.url} width="100" alt="logo" />
+        </div>
+    )
+}
+export default Logo
