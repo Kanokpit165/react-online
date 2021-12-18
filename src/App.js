0338e0163db435acb02bae2a8cb26da3c04c006b
Kanokpit165 <ra.kanokpit_st@tni.ac.th>
2021-12-19 04:12:09 +0700

    Week 5 Test 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
-import NavBar from './components/NavBar'
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
-import Footer from './components/Footer'
-function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/about">
-          <AboutPage />
-        </Route>
-      </Switch>
-      <Footer />
-      </Router>
-  )
-}
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   //Link
+ } from "react-router-dom";
+ import AboutPage from "./pages/AboutPage";
+ import ProductPage from "./pages/ProductPage";
+ import ContactUs from "./pages/ContactUs";
+ import DetailPage from "./pages/DetailPage";
 
-export default App
+ function App() {
+   return (
+    <Router>
+      <NavBar/>
+       <Switch>
+         <Route exact path='/'><HomePage/></Route>
+         <Route path='/about'><AboutPage/></Route>
+         <Route path='/product'><ProductPage/></Route>
+         <Route path='/contact'><ContactUs/></Route>
+         <Route path='/detail'><DetailPage/></Route>
+       </Switch>
+       <Footer/>
+     </Router>
+  );
+}
+export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,46 +1,34 @@
 import React from 'react'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom'
-const NavBar = () => {
-  return (
-    <Navbar bg="dark" variant="dark" expand="md">
-      <Container>
-         {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-         <NavLink to="/" className="navbar-brand" exact>
-          React-Bootstrap
-        </NavLink>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <NavLink
-              className="nav-link"
-              activeClassName="active"
-              to="/"
-              exact>
-              Home
-            </NavLink>
-            <NavLink
-              className="nav-link"
-              activeClassName="active"
-              to="/about"
-              exact>
-              About
-            </NavLink>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
+import {Navbar, Nav, NavDropdown, Container} from 'react-bootstrap'
+import {NavLink} from "react-router-dom"
+function NavBar() {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    {/*<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>*/}
+                    <NavLink className="navbar-brand" exact to="/">React-Boostrap</NavLink>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                         <Nav className="me-auto">
+                             {/*<Nav.Link href="#home">Home</Nav.Link>*/}
+                             <NavLink className="nav-link" activeClassName="active" exact to="/">Home</NavLink>
+                             <NavLink className="nav-link" activeClassName="active" to="/product">Product</NavLink>
+                             {/*<Nav.Link href="#link">Link</Nav.Link>*/}
+                             <NavLink className="nav-link" activeClassName="active" to="/about">About</NavLink>
+                             <NavLink className="nav-link" activeClassName="active" to="/contact">Contact Us</NavLink>
+                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    )
 }
 export default NavBar
