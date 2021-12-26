9430fa03faf5f493a329c1b457c08b4ef964ac98
Kanokpit165 <ra.kanokpit_st@tni.ac.th>
2021-12-26 14:00:30 +0700

    Chapter 4 #2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
 import {
   BrowserRouter as Router,
+  Switch,
   Route,
-  Switch
-} from 'react-router-dom'
-import NavBar from './components/NavBar'
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
-import ProductPage from './pages/ProductPage'
-import DetailPage from './pages/DetailPage'
-import ContactPage from './pages/ContactPage'
-import Footer from './components/Footer'
-function App() {
+  Link
+} from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+ import ProductPage from "./pages/ProductPage";
+ import DetailPage from "./pages/DetailPage";
+ import ContactPage from "./pages/ContactPage";
+ import HospitalPage from "./pages/Hospital/HospitalPage";
+
+
+ function App() {
   return (
     <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/about">
-          <AboutPage />
-        </Route>
-        <Route path="/products">
-          <ProductPage />
-        </Route>
-        <Route path="/detail/:id/title/:title">
-          <DetailPage />
-        </Route>
-        <Route path="/contact">
-          <ContactPage />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
-  )
+        <NavBar/>
+        <Switch>
+          <Route exact path='/'><HomePage/></Route>
+          <Route path='/product'><ProductPage/></Route>
+           <Route path='/about'><AboutPage/></Route>
+           <Route path='/detail/:id/title/:title'><DetailPage/></Route>
+           <Route path='/contact'><ContactPage/></Route>
+           <Route path='/hospital'><HospitalPage/></Route>
+         </Switch>
+         <Footer/>
+     </Router>
+  );
 }
-export default App
+export default App;
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,76 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { Spinner, Card, CardDeck } from 'react-bootstrap'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
+import { Spinner, Card, CardDeck ,Button} from "react-bootstrap";
+import { useParams,useHistory } from "react-router-dom";
 
-const DetailPage = () => {
-  const { id, title } = useParams()
-  const [detail, setDetail] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-  useEffect(async () => {
+ const DetailPage = () => {
+   const { id, title } = useParams();
+   const history = useHistory();
+
+   const [detail, setDetail] = React.useState([]);
+   const [loading, setLoading] = React.useState(false);
+   const [error, setError] = React.useState(null);
+  const getData = async (id) => {
     try {
-      setLoading(true)
-      const res = await axios.get(
-        'https://api.codingthailand.com/api/course/' + id,
-      )
-      setDetail(res.data.data)
+      setLoading(true); //เริ่มหมุน
+      const resp = await axios.get(
+        "https://api.codingthailand.com/api/course/" + id
+      );
+      //console.log(resp.data)
+      setDetail(resp.data.data);
     } catch (error) {
-      setError(error.response.data.message)
-      console.error(error.response)
+      setError(error);
+      console.log(error.resp);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [id])
-
-  if (loading) {
+  };
+  React.useEffect(() => {
+    getData(id);
+  }, [id]);
+  if (loading === true) {
     return (
       <div className="text-center mt-5">
-        <Spinner animation="border" variant="primary" />
+        <Spinner animation="border" variant="warning" />
       </div>
-    )
+    );
   }
   if (error) {
     return (
       <div className="text-center mt-5 text-danger">
-        <h4>Error from API, please try again</h4>
-        <p>{error}</p>
+        <h4>Error from API, plese try again</h4>
+        <p>{error.response.data.message}</p>
       </div>
-    )
+    );
   }
-
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12 mt-4">
-          <h2>DetailPage</h2>
-          <p>id: {id}</p>
-          <p>title: {title}</p>
-          <div className="row">
-            <CardDeck>
-              {detail.length > 0 ? (
-                detail.map((d, index) => {
-                  return (
-                    <div className="col-md-4" key={d.ch_id}>
-                      <Card className="mb-4 shadow-sm¸¸Zˇ">
-                        <Card.Body>
-                          <Card.Title>{d.ch_title}</Card.Title>
-                        </Card.Body>
-                        <Card.Footer>
-                          <small className="text-muted">{d.ch_dateadd}</small>
-                        </Card.Footer>
-                      </Card>
-                    </div>
-                  )
-                })
-              ) : (
-                <h1>No Data</h1>
-              )}
-            </CardDeck>
+          <h2>Detail Page</h2>
+           <p>
+             {title} ({id})
+           </p>
+
+           <Button onClick={()=>{history.goBack()}} variant="primary">Back</Button>
+
+           <div className="row">
+           <CardDeck>
+             {detail.length > 0 ? (
+              detail.map((d, index) => {
+                return (
+                    <div className="col-md-4" key={(d.ch_id)}>
+                        <Card className="mb-4 shadow-sm">
+                            <Card.Body>
+                            <Card.Title>{d.ch_title}</Card.Title>
+                            <Card.Text>
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">
+                                {d.ch_dateadd}
+                            </small>
+                            </Card.Footer>
+                        </Card>
+                  </div>
+                );
+              })
+            ) : (
+              <h1>No Data</h1>
+            )}
+          </CardDeck>
           </div>
         </div>
       </div>
     </div>
-  )
-}
-export default DetailPage
+  );
+};
+export default DetailPage;
