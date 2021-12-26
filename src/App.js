4903e52978dd98305395beba9589f27c82a22b0e
Kanokpit165 <ra.kanokpit_st@tni.ac.th>
2021-12-26 13:49:42 +0700

    Chapter 4 #1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
-import React from "react";
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
-import HomePage from "./pages/HomePage";
+import React from 'react'
+import { render } from 'react-dom'
 import {
-   BrowserRouter as Router,
-   Switch,
-   Route,
-   //Link
- } from "react-router-dom";
- import AboutPage from "./pages/AboutPage";
- import ProductPage from "./pages/ProductPage";
- import ContactUs from "./pages/ContactUs";
- import DetailPage from "./pages/DetailPage";
-
- function App() {
-   return (
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ProductPage from './pages/ProductPage'
+import DetailPage from './pages/DetailPage'
+import ContactPage from './pages/ContactPage'
+import Footer from './components/Footer'
+function App() {
+  return (
     <Router>
-      <NavBar/>
-       <Switch>
-         <Route exact path='/'><HomePage/></Route>
-         <Route path='/about'><AboutPage/></Route>
-         <Route path='/product'><ProductPage/></Route>
-         <Route path='/contact'><ContactUs/></Route>
-         <Route path='/detail'><DetailPage/></Route>
-       </Switch>
-       <Footer/>
-     </Router>
-  );
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
+        <Route path="/products">
+          <ProductPage />
+        </Route>
+        <Route path="/detail/:id/title/:title">
+          <DetailPage />
+        </Route>
+        <Route path="/contact">
+          <ContactPage />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
-export default App;
+export default App
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,15 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Spinner, Card, CardDeck } from 'react-bootstrap'
+import axios from 'axios'
 
- const DetailPage = () => {
-     return (
-         <div className="container">
-             <div className="row">
-                 <div className="col-md-12 mt-2">
-                     <h2>Detail Page</h2>
-                 </div>
-             </div>
-         </div>
-     )
- }
+const DetailPage = () => {
+  const { id, title } = useParams()
+  const [detail, setDetail] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  useEffect(async () => {
+    try {
+      setLoading(true)
+      const res = await axios.get(
+        'https://api.codingthailand.com/api/course/' + id,
+      )
+      setDetail(res.data.data)
+    } catch (error) {
+      setError(error.response.data.message)
+      console.error(error.response)
+    } finally {
+      setLoading(false)
+    }
+  }, [id])
 
- export default DetailPage
+  if (loading) {
+    return (
+      <div className="text-center mt-5">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    )
+  }
+  if (error) {
+    return (
+      <div className="text-center mt-5 text-danger">
+        <h4>Error from API, please try again</h4>
+        <p>{error}</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 mt-4">
+          <h2>DetailPage</h2>
+          <p>id: {id}</p>
+          <p>title: {title}</p>
+          <div className="row">
+            <CardDeck>
+              {detail.length > 0 ? (
+                detail.map((d, index) => {
+                  return (
+                    <div className="col-md-4" key={d.ch_id}>
+                      <Card className="mb-4 shadow-sm¸¸Zˇ">
+                        <Card.Body>
+                          <Card.Title>{d.ch_title}</Card.Title>
+                        </Card.Body>
+                        <Card.Footer>
+                          <small className="text-muted">{d.ch_dateadd}</small>
+                        </Card.Footer>
+                      </Card>
+                    </div>
+                  )
+                })
+              ) : (
+                <h1>No Data</h1>
+              )}
+            </CardDeck>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default DetailPage
