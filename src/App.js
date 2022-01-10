e6139a51fe739eba815fdb9deb08cb5618a8c9ce
Kanokpit165 <ra.kanokpit_st@tni.ac.th>
2022-01-10 22:21:08 +0700

    Chapter 5 #2

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ import {
 } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import ProductPage from "./pages/ProductPage";
-import DetailPage from "./pages/DetailPage";
-import ContactPage from "./pages/ContactPage";
-import HospitalPage from "./pages/Hospital/HospitalPage";
-import CategoryPage from './pages/Category/CategoryPage'
+ import DetailPage from "./pages/DetailPage";
+ import ContactPage from "./pages/ContactPage";
+ import HospitalPage from "./pages/Hospital/HospitalPage";
+ import CategoryPage from './pages/Category/CategoryPage';
+ import EditPage from "./pages/Category/EditPage";
 
 
-function App() {
+ function App() {
   return (
     <Router>
         <NavBar/>
@@ -24,13 +25,20 @@ function App() {
           <Route exact path='/'><HomePage/></Route>
           <Route path='/product'><ProductPage/></Route>
           <Route path='/about'><AboutPage/></Route>
-          <Route path='/detail/:id/title/:title'><DetailPage/></Route>
-          <Route path='/contact'><ContactPage/></Route>
-          <Route path='/hospital'><HospitalPage/></Route>
-          <Route path="/category"><CategoryPage /></Route>
-        </Switch>
-        <Footer/>
-    </Router>
+           <Route path='/detail/:id/title/:title'><DetailPage/></Route>
+           <Route path='/contact'><ContactPage/></Route>
+           <Route path='/hospital'><HospitalPage/></Route>
+            {/*<Route path='/category'><indexPage/></Route>*/}
+            <Route path='/category' render={ ({match : {url}}) => (
+             <>
+             <Route path={`${url}/`} exact><IndexPage></IndexPage></Route>
+             <Route path={`${url}/create`}><CreatePage></CreatePage></Route>
+             <Route path={`${url}/edit/:id`}><EditPage></EditPage></Route>
+             </>
+           )}></Route>
+         </Switch>
+         <Footer/>
+     </Router>
   );
 }
 export default App;
