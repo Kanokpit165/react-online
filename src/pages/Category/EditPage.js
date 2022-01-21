9ddc9001c6499ffcf7e816e06ca341fae1bd421d
Kanokpit165 <ra.kanokpit_st@tni.ac.th>
2022-01-21 22:18:44 +0700

    Chapter 6 #2

--- src/pages/Category/EditPage.js
+++ src/pages/Category/EditPage.js
@@ -5,52 +5,49 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 import axios from 'axios';
 import {useParams ,useHistory} from 'react-router-dom'
-
-
 const schema = yup.object({
     name: yup.string().required('Category news cannot be null'),
   }).required();
-
 const EditPage = () => {
-
     const [error, setError] = React.useState(null)
-
     const { id } = useParams()
 
-    const history = useHistory()
+     const history = useHistory()
 
-    const { register, handleSubmit, formState:{ errors } } = useForm({
+     const { register, handleSubmit, formState:{ errors }, setValue } = useForm({
         resolver: yupResolver(schema)
       });
 
     const onSubmit = async (data) => {
         console.log(data)
-        // try{
-        //     const apiURL = 'https://api.codingthailand.com/api/category'
-        //     const resp = await axios.post(apiURL,
-        //         {
-        //             name: data.name
-        //         }
-        //     )
-        //     alert(resp.data.message)
-        //     history.goBack()
-        // }
-        // catch(error){
-        //     setError(error)
-        // }
+        try{
+            const apiURL = 'https://api.codingthailand.com/api/category'
+            const resp = await axios.put(apiURL,
+                {
+                    id:id,
+                    name: data.name
+                }
+            )
+            alert('updateเสร็จสิ้น')
+            history.goBack()
+        }
+        catch(error){
+            setError(error)
+        }
     }
 
     React.useEffect(() => {
-        getData(id);
-    },[id])
+       getData(id);
+   },[id])
 
     const getData = async (id) => {
         const resp = await axios.get('https://api.codingthailand.com/api/category/' + id);
-        console.log(resp.data)
-    }
+         //console.log(resp.data)
+         setValue('name',resp.data.name)
+     };
 
 
-    if(error){
+     if(error){
         return(
             <div className="text-center mt-5 text-danger">
                 <h4>Error from API, please try again</h4>
@@ -58,12 +55,11 @@ const EditPage = () => {
             </div>
         )
     }
-
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-12 mt-2">
-                <h2>Edit Category</h2>
+                    <h2>Edit Category</h2>
                     <Form onSubmit={handleSubmit(onSubmit)}>
                         <Form.Group controlId="name">
                             <Form.Label>Category New</Form.Label>
@@ -75,16 +71,15 @@ const EditPage = () => {
                                     </Form.Control.Feedback>
                                 )
                             }
-                        </Form.Group>
+                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </div>
+                         <Button variant="primary" type="submit">
+                         Update
+                         </Button>
+                     </Form>
+                 </div>
             </div>
         </div>
     )
 }
-
 export default EditPage
